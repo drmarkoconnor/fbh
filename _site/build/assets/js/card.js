020696ff230e6e7c68e7932e6714bd120301b759
Dr Mark O'Connor <dr.mark.oconnor@googlemail.com>
2021-05-25 20:51:49 +0100

    removeed build from code kit

--- _site/build/assets/js/card.js
+++
@@ -1,1 +0,0 @@
-const header=document.querySelector("header");function fixnav(){window.scrollY>10?header.classList.add("active"):header.classList.remove("active")}window.addEventListener("scroll",fixnav);
